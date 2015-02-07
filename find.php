505cec83ebdebbac67aacb2f1edd5bc8004b8d5b
xtream-core <x98hacker@gmail.com>
2015-02-07 13:15:06 +0000

    find.php
    The page which asks for the UID and posts it to raw.php/view.php

---
+++ find.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<link rel="icon" type="image/png" href="/logo_base.png" sizes="16x16">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>YOURNAME - CodeShare</title>
+</head>
+
+<body>
+<fieldset>
+<legend>Find Code</legend>
+<form action="raw.php" method="post">
+ <p>Your Unique ID: <input type="text" name="fndcode" class="uname" /></p>
+ <p><input type="submit" /></p>
+ </fieldset>
+</form>
+</body>
+
+</html>
